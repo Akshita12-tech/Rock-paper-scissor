62f28a88dcb46da839f3964ac929f56fc29fff9b
Akshita12-tech <akshitajamindar@gmail.com>
2025-07-14 20:07:13 +0100

    Update rps game.py

--- rps game.py
+++ rps game.py
@@ -1,12 +1,12 @@
 import random
 def play_game():
-  choices=[Rock, Paper, Scissor]
-  while true:
-    user_choice= input("Enter a choice from Rock, Paper, Scissor or Q to quit the game").lower()
-    if user_choice == q:
-      print("Thanks for playing")
+  choices=["rock", "paper", "scissors"]
+  while True:
+    user_choice= input("Enter a choice from Rock, Paper, Scissors or Q to quit the game").lower()
+    if user_choice == "q":
+      print("Thanks for playing!")
       break 
-    for user_choice not in choices:
+    if user_choice not in choices:
       print("Re-enter, the choice does not exists")
       continue
     
@@ -14,11 +14,13 @@ def play_game():
     print(f"Computer's choice is: {computer_choice}")
     if user_choice== computer_choice:
       print("It's a tie")
-    elif ((user_choice="rock" and  computer_choice="scissor") or
-    (user_choice="scissor" and computer_choice="paper") or 
-    (user_choice="paper" and computer_choice="rock")):
+    elif ((user_choice=="rock" and  computer_choice=="scissors") or
+    (user_choice=="scissors" and computer_choice=="paper") or 
+    (user_choice=="paper" and computer_choice=="rock")):
       print("You are the winner")
-else:
-  print("The computer has won so better luck next time")
-    
-    
+    else:
+      print("The computer has won so better luck next time")
+  
+ 
+play_game()   
+
