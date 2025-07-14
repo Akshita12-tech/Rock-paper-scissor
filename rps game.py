8d161adbdb07d766d354aeeaf74393c80b1ad20b
Akshita12-tech <akshitajamindar@gmail.com>
2025-07-14 19:49:12 +0100

    Update rps game.py

--- rps game.py
+++ rps game.py
@@ -14,4 +14,11 @@ def play_game():
     print(f"Computer's choice is: {computer_choice}")
     if user_choice== computer_choice:
       print("It's a tie")
-    elif (user_choice="rock" and  computer_choice="scissor") or (
+    elif ((user_choice="rock" and  computer_choice="scissor") or
+    (user_choice="scissor" and computer_choice="paper") or 
+    (user_choice="paper" and computer_choice="rock")):
+      print("You are the winner")
+else:
+  print("The computer has won so better luck next time")
+    
+    
