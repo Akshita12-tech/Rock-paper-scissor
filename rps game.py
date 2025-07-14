7450d10752a87b43ab793d498275be25ab5d91f1
Akshita12-tech <akshitajamindar@gmail.com>
2025-07-14 13:18:29 +0100

    Create rps game.py

---
+++ rps game.py
@@ -0,0 +1,17 @@
+import random
+def play_game():
+  choices=[Rock, Paper, Scissor]
+  while true:
+    user_choice= input("Enter a choice from Rock, Paper, Scissor or Q to quit the game").lower()
+    if user_choice == q:
+      print("Thanks for playing")
+      break 
+    for user_choice not in choices:
+      print("Re-enter, the choice does not exists")
+      continue
+    
+    computer_choice = random.choice(choices)
+    print(f"Computer's choice is: {computer_choice}")
+    if user_choice== computer_choice:
+      print("It's a tie")
+    elif (user_choice="rock" and  computer_choice="scissor") or (
